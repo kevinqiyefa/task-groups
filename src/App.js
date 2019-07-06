67146eae447a59ca7f933dc8a760a72b3a1a1fbb
kevinqiyefa <kevinqiyf@hotmail.com>
2019-07-06 09:46:10 -0700

    add uuid and react router related functionalities

--- src/App.js
+++ src/App.js
@@ -1,8 +1,112 @@
-import React, { Component } from 'react'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-export default class App extends Component {
-  render() {
-    return null
+import TaskGroups from './components/TaskGroups';
+import Group from './components/TaskGroup';
+import { getTaskData as mockAPI } from './services/api';
+import Loader from './components/Loader';
+
+import './App.css';
+
+function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [taskGroups, setTaskGroups] = useState({});
+
+  const generateGroups = data => {
+    const groups = {};
+    data.forEach(task => {
+      const taskObj = {
+        id: task.id,
+        task: task.task,
+        dependencyIds: task.dependencyIds,
+        completedAt: task.completedAt
+      };
+      if (groups.hasOwnProperty(task.group)) {
+        groups[task.group].push(taskObj);
+      } else {
+        groups[task.group] = [taskObj];
+      }
+    });
+
+    setTaskGroups(groups);
+  };
+
+  const checkDependencyTaskComplete = depIds => {
+    const idsSet = new Set(depIds);
+
+    for (let tG in taskGroups) {
+      for (let task of taskGroups[tG]) {
+        if (idsSet.has(task.id) && !task.completedAt) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  };
+
+  const handleTaskChange = (groupName, taskId) => {
+    let updatedGroup = [...taskGroups[groupName]];
+
+    updatedGroup = updatedGroup.map(task => {
+      if (task.id === taskId) {
+        if (task.completedAt) {
+          task.completedAt = null;
+        } else if (
+          (task.dependencyIds.length > 0 &&
+            checkDependencyTaskComplete(task.dependencyIds)) ||
+          task.dependencyIds.length < 1
+        ) {
+          task.completedAt = new Date().toUTCString();
+        }
+      }
+      return task;
+    });
+
+    setTaskGroups({ ...taskGroups, [groupName]: updatedGroup });
+  };
+
+  async function fetchTaskData() {
+    setIsLoading(true);
+    try {
+      const data = await mockAPI();
+      generateGroups(data);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      //handle error message
+    }
   }
+
+  useEffect(() => {
+    fetchTaskData();
+  }, []);
+
+  if (isLoading) return <Loader />;
+
+  return (
+    <BrowserRouter>
+      <div style={{ textAlign: 'center' }}>
+        <Route
+          exact
+          path="/"
+          render={() => <TaskGroups taskData={taskGroups} />}
+        />
+        <Route
+          exact
+          path="/:taskGroup"
+          render={props => (
+            <Group
+              taskData={taskGroups}
+              handleTaskChange={handleTaskChange}
+              checkDependencyTaskComplete={checkDependencyTaskComplete}
+              {...props}
+            />
+          )}
+        />
+      </div>
+    </BrowserRouter>
+  );
 }
+
+export default React.memo(App);
